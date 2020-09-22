82bee3b577101406b6a5c448a507841315025fac
FreekAlberti <federicoalberi96@gmail.com>
2020-09-22 15:46:06 +0200

    WIP ajax

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+var ricerca = "ritorno al futuro";
+  $.ajax(
+    {
+      "url": "https://api.themoviedb.org/3/search/movie",
+      "data": {
+        "api_key" : "e809f4ea095436f35151e7d0e1a01040",
+        "query" : ricerca
+      },
+      "method": "GET",
+      "success": function (data, stato) {
+        console.log(data);
+      },
+      "error": function (richiesta, stato, errori) {
+        console.log(errori);
+      }
+    }
+  );
 
   // e809f4ea095436f35151e7d0e1a01040
+
   // Handlebars
   var source = $("#film-template").html();
   var template = Handlebars.compile(source);
