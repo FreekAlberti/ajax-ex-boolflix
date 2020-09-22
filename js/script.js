f5997265eb02a43ae76cad525ab27a5aa807918f
FreekAlberti <federicoalberi96@gmail.com>
2020-09-22 13:11:48 +0200

    WIP handlebar

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
 
-  
+  // e809f4ea095436f35151e7d0e1a01040
+  // Handlebars
+  var source = $("#film-template").html();
+  var template = Handlebars.compile(source);
+  var context = {
+    title: "My New Post",
+    body: "This is my first post!"
+  };
+  var html = template(context);
+  $("#lista-film").append(html);
 });
