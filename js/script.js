b2ad2bbe5bdfe66061ed5a8c48ad2dd7cf2f4e27
FreekAlberti <federicoalberi96@gmail.com>
2020-09-23 17:27:52 +0200

    chiamata serie tv

--- js/script.js
+++ js/script.js
@@ -1,13 +1,72 @@
 $(document).ready(function() {
   $(".btn").click(function() {
-    chiamataAjax();
+    $("h1").show();
+    removeLi();
+    chiamataAjaxTv();
+    chiamataAjaxFilm();
   });
   $(document).keydown(function(e){
     if (e.keyCode == 13) {
-      chiamataAjax();
+      $("h1").show();
+      removeLi();
+      chiamataAjaxTv();
+      chiamataAjaxFilm();
     }
   });
 });
+
+
+// FUNCTION
+
+// TV function
+
+function risultatiTv(tvAPI, ricerca) {
+  console.log(tvAPI);
+  console.log(ricerca);
+  var source = $("#tv-template").html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < tvAPI.length; i++) {
+    var name = tvAPI[i].name;
+    var originalName = tvAPI[i].original_name;
+    var lan = tvAPI[i].original_language;
+    var voto = tvAPI[i].vote_average;
+    var votoIntero = votoInt(voto);
+    var starRating = stars(votoIntero);
+    var context = {
+      "name" : name,
+      "original_name" : originalName,
+      "original_language" : lan,
+      "vote_average" : starRating
+    };
+    var html = template(context);
+    $("#lista-tv").append(html);
+  }
+}
+
+function chiamataAjaxTv() {
+  var ricerca = $(".input-titolo").val();
+  $.ajax(
+    {
+      "url": "https://api.themoviedb.org/3/search/tv",
+      "data": {
+        "api_key" : "e809f4ea095436f35151e7d0e1a01040",
+        "query" : ricerca,
+        "language" : "it-IT",
+        "include_adult" : false
+      },
+      "method": "GET",
+      "success": function (data, stato) {
+        var tvAPI = data.results;
+        risultatiTv(tvAPI, ricerca);
+      },
+      "error": function (richiesta, stato, errori) {
+        console.log(errori);
+      }
+    }
+  );
+}
+
+// Film function
 
 function risultatiFilm(filmAPI, ricerca) {
   var source = $("#film-template").html();
@@ -31,7 +90,7 @@ function risultatiFilm(filmAPI, ricerca) {
   $(".input-titolo").val("");
 }
 
-function chiamataAjax() {
+function chiamataAjaxFilm() {
   var ricerca = $(".input-titolo").val();
   $.ajax(
     {
@@ -45,9 +104,6 @@ function chiamataAjax() {
       "method": "GET",
       "success": function (data, stato) {
         var filmAPI = data.results;
-        $("li").each(function() {
-          $(this).remove();
-        });
         risultatiFilm(filmAPI, ricerca);
       },
       "error": function (richiesta, stato, errori) {
@@ -56,6 +112,8 @@ function chiamataAjax() {
     }
   );
 }
+
+// funzioni genriche
 
 function votoInt(voto) {
   var votoCinque = voto / 2;
@@ -73,4 +131,10 @@ function stars(votoIntero) {
     }
   }
   return starRating;
+}
+
+function removeLi() {
+  $("li").each(function() {
+    $(this).remove();
+  });
 }
